22292a7df07aee014c9b197408d20ec6f9d5ae16
hzlzh <hzlzh.dev@gmail.com>
2013-01-17 11:42:16 +0800

    Google code prettify added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function($) {
+console.log('111')
 
+setTimeout(function() {
+setTimeout(function() {
+$('pre,code').addClass('prettyprint')
+prettyPrint()
+
+}, 1000);
+
+console.log('222')
+}, 1000);
 });
+console.log('333')
