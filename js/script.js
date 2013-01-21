7baddb6b29e49b9ec3c7de9c054f27bd923d5bbe
hzlzh <hzlzh.dev@gmail.com>
2013-01-21 19:33:20 +0800

    change js pretty

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 jQuery(document).ready(function(){
 
-setTimeout(function(){
-	$('pre, code:not(pre code)').addClass('prettyprint');
-	prettyPrint();
-}, 1000);
-
-$(function(){
-    window.onscroll=function(){
-        if(document.body.scrollTop>70 || document.documentElement.scrollTop>70){
-            $(".container nav").addClass('fixed');
-        }
-        else{
-            $(".container nav").removeClass('fixed');
-        }
+    var pp = function(t){
+        setTimeout(function(){
+            $('pre, code:not(pre code)').addClass('prettyprint');
+            prettyPrint();
+        }, t);
     }
-});
-
-
-
+    pp(1000);
+    
+    $('#paneSetting').change(function(){
+        if($(this).val() == 'previewPane'){
+            pp(1000);
+        }
+    });
+    
+    $(function(){
+        window.onscroll=function(){
+            if(document.body.scrollTop>70 || document.documentElement.scrollTop>70){
+                $(".container nav").addClass('fixed');
+            }
+            else{
+                $(".container nav").removeClass('fixed');
+            }
+        }
+    });
 
 });
